b0a79627e28915e72786c8c19b347aec734cf523
brightdartey <darteyb28@gmail.com>
2023-07-19 10:57:56 +0000

    added pop-up and FAQ projects

---
+++ Make a Pop-Up modal in js/app.js
@@ -0,0 +1,12 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector(".modal-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+const modalClose = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click", () => modalOverlay.classList.add("open-modal"));
+
+modalClose.addEventListener("click", () => modalOverlay.classList.remove("open-modal"));
